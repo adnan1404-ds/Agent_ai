0aee311fe3cb315bda490798219154a7b41df3a0
Sardar Adnan <adnan1404.datascience@gmail.com>
2025-12-01 23:01:06 +0500

    Delete projects directory

--- projects/app.py
+++
@@ -1,8 +0,0 @@
-from project.main_agent import run_agent
-import gradio as gr
-
-def process_input(text):
-    return run_agent(text)
-
-iface = gr.Interface(fn=process_input, inputs="text", outputs="text")
-iface.launch()
